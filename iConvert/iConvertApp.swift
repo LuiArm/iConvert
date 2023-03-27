bf5b4eb377bd26ba1420d307cccb385bb0dee43d
LuiArm <Luisarmendariz24@icloud.com>
2023-03-27 14:35:59 -0600

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by luis armendariz on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
